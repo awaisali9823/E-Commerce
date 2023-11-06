cc8382283459b3160ad32107543f2902b642ea43
Awais Ali <awaisali9823@gmail.com>
2023-11-06 15:51:49 +0500

    Moved DB url to env file

--- Server/index.js
+++ Server/index.js
@@ -1,12 +1,14 @@
 import express from "express"
 import mongoose from "mongoose"
 import bodyParser from "body-parser"
+import dotenv from "dotenv"
 import cors from 'cors'
 import { router } from "./routes/userRoutes.js";
 
 
 const app = express();
 app.use(bodyParser.json())
+dotenv.config();
 //app.use(cors())
 app.use(cors({
     origin:"http://localhost:3000",
@@ -16,10 +18,10 @@ app.use(cors({
 
 
 app.use("", router)
-const port = 4000;
+const port = process.env.Port;
 
-mongoose.connect('mongodb+srv://awaisali9823:Awaisali@firstdb.iekrqrc.mongodb.net/E-Commerce-Project')
 
+mongoose.connect(process.env.mongodb);
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 })
